d614792bce8d6d5ab145927dd62cf7f892eb49cd
Mr. WPress <72110367+mrwpress@users.noreply.github.com>
2023-02-01 10:49:18 -0700

    Updating to GA4

--- functions.php
+++ functions.php
@@ -45,22 +45,20 @@ function aclstrong_ga_code() {
 	?>
 
 
-  <!-- GA Google Analytics @ https://m0n.co/ga -->
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q63GP2V3GK"></script>
   <script>
-    (function( i, s, o, g, r, a, m ) {
-      i[ 'GoogleAnalyticsObject' ] = r;
-      i[ r ] = i[ r ] || function() {
-        (i[ r ].q = i[ r ].q || []).push( arguments )
-      }, i[ r ].l = 1 * new Date();
-      a = s.createElement( o ),
-        m = s.getElementsByTagName( o )[ 0 ];
-      a.async = 1;
-      a.src   = g;
-      m.parentNode.insertBefore( a, m )
-    })( window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga' );
-    ga( 'create', 'UA-102354581-1', 'auto' );
-    ga( 'send', 'pageview' );
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push( arguments );
+    }
+
+    gtag( 'js', new Date() );
+
+    gtag( 'config', 'G-Q63GP2V3GK' );
   </script>
+
 
   <!-- Deadline Funnel -->
   <script type="text/javascript" data-cfasync="false">function SendUrlToDeadlineFunnel( e ) {
@@ -98,17 +96,3 @@ function aclstrong_ga_code() {
   <meta name="facebook-domain-verification" content="fvn14sxw8fzl11cnb48aguw0waydmv"/>
 	<?php
 }
-
-function aclstrong_search_filter( $query ) {
-	// If this is the admin side we do no filtering
-	if ( is_admin() ) {
-		return $query;
-	}
-
-	if ( $query->is_search ) {
-		$query->set( 'post_type', [ 'post', 'page' ] );
-	}
-	return $query;
-}
-
-add_filter( 'pre_get_posts', 'aclstrong_search_filter' );
